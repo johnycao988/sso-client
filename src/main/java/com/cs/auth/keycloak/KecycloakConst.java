98e931f0ab2ba181e2ddd8acbc75ce7dd18856d1
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2018-01-17 17:09:44 +0800

    11

--- src/main/java/com/cs/auth/keycloak/KecycloakConst.java
+++ src/main/java/com/cs/auth/keycloak/KecycloakConst.java
@@ -6,6 +6,21 @@ public class KecycloakConst {
 	public static final String CLIENT_SECRET="client_secret";
 	public static final String CODE="code";
 	public static final String SESSION_STATE="session_state";
+	public static final String REDIRECT_URL="redirect_uri";
+	
+	
 	public static final String KEYCLOAK_SESSION_NAME="KEYCLOAK_SESSION_NAME"; 
 	public static final String KEYCLOAK_COOKIES_SESSION_DATA="KEYCLOAK_COOKIES_SESSION_DATA";  
+	
+	public static final String REQ_HEADER_AUTHORIZATION="Authorization";
+	public static final String REQ_HEADER_CONTENT_TYPE="Content-Type";
+	public static final String REQ_HEADER_GRANT_TYPE="grant_type";
+	
+	public static final String REQ_HEADER_VALUE_AUTHORIZATION_CODE="authorization_code";
+	public static final String REQ_HEADER_VALUE_BAISC=" Basic ";
+	public static final String REQ_HEADER_VALUE_BEARER=" Bearer ";
+	public static final String REQ_HEADER_VALUE_CONTENT_TYPE_FORM_URL_ENCODEED="application/x-www-form-urlencoded";
+	
+	
+	
 }
